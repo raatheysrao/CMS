bbf7e8d0220d7e2e60d34d65c178614356ddbdee
RAATHEYS RAO RAMA RAO <raatheysrao@gmail.com>
2025-01-05 02:20:05 +0800

    db set

--- users/includes/config.php
+++ users/includes/config.php
@@ -1,7 +1,7 @@
 <?php
-$mysql_hostname = "35.202.195.238";
-$mysql_user = "cms_ums";
-$mysql_password = "cms@1234";
+$mysql_hostname = "localhost";
+$mysql_user = "root";
+$mysql_password = "";
 $mysql_database = "cms";
 $bd = mysqli_connect($mysql_hostname, $mysql_user, $mysql_password, $mysql_database) or die("Could not connect database");
 ?>
